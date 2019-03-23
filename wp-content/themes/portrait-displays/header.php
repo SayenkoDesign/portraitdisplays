f05c2a1a68fcafd3fca990ea941e8bf1fa4ca39b
Michael Heimes <michael@cairndigitalmedia.com>
2019-03-23 08:13:53 -0400

    Contact Form, Modal

--- wp-content/themes/portrait-displays/header.php
+++ wp-content/themes/portrait-displays/header.php
@@ -27,6 +27,15 @@
 </head>
 
 <body <?php body_class(); ?>>
+	
+	<div class="reveal" id="contact-modal" data-animation-in="fadeIn fast" data-animation-out="fadeOut fast" data-reveal>
+	<div id="contact-modal-header"><h3>Let's Talk</h3>
+		<div class="cm-close-button" data-close aria-label="Close reveal"><img class="top-arrow" src="/wp-content/themes/portrait-displays/assets/svg/contact-modal-close.svg"/></div>
+	</div>
+		<?php
+			_s_get_template_part( 'template-parts/global', 'contact-form' );
+		?>
+	</div>
 
     <ul class="skip-link screen-reader-text">
         <li><a href="#content" class="screen-reader-shortcut"><?php esc_html_e( 'Skip to content', '_s' ); ?></a></li>
@@ -91,7 +100,7 @@
                 <nav id="site-navigation" class="nav-primary columns" role="navigation" aria-label="Main" itemscope itemtype="https://schema.org/SiteNavigationElement">  
 	                          
 					<div id="upper-nav">
-							<a id="contact-modal-button" href="#"><span class="upper-nav-skew-correct"><span class="upper-link-text"><?php the_field('lets_talk_label', 'option');?></span></span></a>
+							<a id="contact-modal-button" data-open="contact-modal" href="#"><span class="upper-nav-skew-correct"><span class="upper-link-text"><?php the_field('lets_talk_label', 'option');?></span></span></a>
 							<?php 
 							$link = get_field('store_link', 'option');
 							if( $link ): 
--- wp-content/themes/portrait-displays/page-templates/contact.php
+++ wp-content/themes/portrait-displays/page-templates/contact.php
@@ -16,23 +16,26 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 
-    <main id="main" class="site-main row columns" role="main">
+    <main id="main" class="site-main" role="main">
 	    
-	    <a id="back-to-previous-link" href="javascript:history.back()"><img src="/wp-content/themes/portrait-displays/assets/svg/back-arrow.svg"/><span id="back-to-previous-text">Back to website</span></a>
+
 	    
-	    		<div class="row">
 
 				<div class="row columns small-12 medium-12 large-5">
+					<a id="back-to-previous-link" href="javascript:history.back()"><img src="/wp-content/themes/portrait-displays/assets/svg/back-arrow.svg"/><span id="back-to-previous-text">Back to website</span></a>
+					
+					<h1 class="text-center"><?php the_field('heading');?></h1>
+					<h4 class="text-center"><?php the_field('subtitle');?></h4>
+					
 					<?php
 					_s_get_template_part( 'template-parts/global', 'contact-form' );
 					?>
 				</div>
 				
-				<div class="google-map">
-		
+				<div class="google-map colums large-7">
+					<?php echo do_shortcode('[wpgmza id="1"]');?>
 				</div>
    
-	    		</div>
     
     </main>
 
